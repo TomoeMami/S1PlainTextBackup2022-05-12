e4ff787f4df42c5816bbcbdb31882abd02ca0ede
TomoeMami <trembleafterme@outlook.com>
2022-08-04 11:27:01 +0800

    [2022-08-04 Thu 11:27:12]

--- autorun.py
+++ autorun.py
@@ -8,7 +8,7 @@ import io
 import os
 import json
 
-old_number = 2083200+int((int(time.time())-1659322737)/86400)*175
+old_number = 2084000+int((int(time.time())-1659583612)/86400)*175
 
 def parse_html(html,threadict):
     # soup = BeautifulSoup(html,from_encoding="utf-8",features="lxml")
